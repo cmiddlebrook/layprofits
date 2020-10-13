addd2de0144bde7799e2b24c1830dd6cef86c67d
Caroline Middlebrook <caroline.middlebrook@googlemail.com>
2020-10-13 22:25:00 +0100

    1) Only store profitable subsets 2) Added more detail in the recording and reporting of results

--- classes/ProfitAnalyser.php
+++ classes/ProfitAnalyser.php
@@ -54,38 +54,55 @@ class ProfitAnalyser
         // now analyse on a per-day basis
         $this->analyseProfitOverRangeOfTargets();
 
+        // print the best results
         ksort($this->profitSubsets);
-        print_r($this->profitSubsets);
+        $numSubsets = count($this->profitSubsets);
+        $mostProfitableSubsets = array_slice($this->profitSubsets, $numSubsets-5);
+        foreach($mostProfitableSubsets as $subset)
+        {
+            $subset->print();
+        }
     }
 
     private function analyseProfitOverRangeOfTargets()
     {
+        // try 10,000 combinations of profit and loss targets to find the most profitable combination
+        $count = 0;
         for($profitTarget = 1; $profitTarget <= 100; $profitTarget += 1)
         {
-            for($stopLoss = -1; $stopLoss >= -100; $stopLoss -= 1)
+            for($stopLoss = -100; $stopLoss <= -1; $stopLoss += 1)
             {
+                $profitSubset = new ProfitSubset($profitTarget, $stopLoss);
                 $numberOfDays = 0;
                 $totalProfit = 0.0;
                 foreach ($this->daysProfits as $day => $profits)
                 {
-                    $totalProfit += $this->analyseDailyProfit($day, $profitTarget, $stopLoss);
+                    $totalProfit += $this->analyseDailyProfit($day, $profitSubset);
                     $numberOfDays++;
                 }
 
-                $profitSubset = new ProfitSubset($profitTarget, $stopLoss, $totalProfit, $numberOfDays);
-                $this->profitSubsets[$totalProfit] = $profitSubset;
+                // only keep the profitable subsets
+                if ($totalProfit > 0.0)
+                {
+                    $count++;
+                    $this->profitSubsets[$totalProfit] = $profitSubset;
+                }
             }
         }
+        echo count($this->profitSubsets) . "subsets analysed\n";
     }
 
-    private function analyseDailyProfit(string $startString, int $profitTarget, int $stopLoss)
+    private function analyseDailyProfit(string $startString, ProfitSubset $profitSubset)
     {
-        $nfmt = new NumberFormatter("en", NumberFormatter::CURRENCY);
+        $profitTarget = $profitSubset->getProfitTarget();
+        $stopLoss = $profitSubset->getStopLoss();
+
+        // $nfmt = new NumberFormatter("en", NumberFormatter::CURRENCY);
         $runningTotal = 0.0;
         $raceProfits = $this->daysProfits[$startString];
         foreach ($raceProfits as $raceProfit)
         {
-            $profit = $raceProfit['race_profit'];
+            $profit = round($raceProfit['race_profit'],2);
             $runningTotal += $profit;
 
             if ($runningTotal >= $profitTarget)
@@ -99,6 +116,7 @@ class ProfitAnalyser
             }
         }
 
+        $profitSubset->addDaysProfit($runningTotal);
         return $runningTotal;
     }
 
--- classes/ProfitSubset.php
+++ classes/ProfitSubset.php
@@ -4,19 +4,54 @@ class ProfitSubset
 {
     private $profitTarget = 0;
     private $stopLoss = 0;
-    private $profit = 0.0;
-    private $numberOfDays = 0;
+    private $profitsByDay = array();
+    private $totalProfit = 0.0;
+    private $highestLoss = 0.0;
+    private $numberOfProfitableDays = 0;
 
-    public function __construct(int $profitTarget, int $stopLoss, float $profit, int $numberOfDays)
+    public function __construct(int $profitTarget, int $stopLoss)
     {
         $this->profitTarget = $profitTarget;
         $this->stopLoss = $stopLoss;
-        $this->profit = $profit;
-        $this->numberOfDays = $numberOfDays;
+    }
+
+    public function getProfitTarget()
+    {
+        return $this->profitTarget;
+    }
+
+    public function getStopLoss()
+    {
+        return $this->stopLoss;
     }
 
     public function getProfit()
     {
         return $this->profit();
+    }
+
+    public function addDaysProfit(float $profit)
+    {
+        array_push($this->profitsByDay, $profit);
+        $this->totalProfit += $profit;
+
+        if ($profit < $this->highestLoss)
+        {
+            $this->highestLoss = $profit;
+        }
+
+        if ($profit > 0)
+        {
+            $this->numberOfProfitableDays++;
+        }
+    }
+
+    public function print()
+    {
+        $numDays = count($this->profitsByDay);
+        $percentageProfitable = 100 / $numDays * $this->numberOfProfitableDays;
+        echo "Profit Target: $this->profitTarget, Stop Loss: $this->stopLoss, Profit: $this->totalProfit\n";
+        echo "Highest Loss: $this->highestLoss, Days Analysed: $numDays, Profitable: $this->numberOfProfitableDays ($percentageProfitable%)\n";
+        print_r($this->profitsByDay);
     }
 }
